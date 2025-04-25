77877c23e3465804d96dd7c378dac1b56892d433
Rambalak-Chaudhary <rambalakchaudhary111@gmail.com>
2025-04-25 14:11:46 +0530

    DFS .java

--- DFS .java
+++ DFS .java
@@ -2,7 +2,6 @@ import java.util.Stack;
 
 public class DFS {
     
-    // Function to perform DFS
     public static boolean dfs(int[][] graph, int startNode, int targetNode) {
         int n = graph.length; // Number of nodes in the graph
         boolean[] visited = new boolean[n]; // Array to track visited nodes
@@ -13,16 +12,16 @@ public class DFS {
         while (!stack.isEmpty()) {
             int currentNode = stack.pop();
             
-            // If the target node is found, return true
+          
             if (currentNode == targetNode) {
                 return true;
             }
             
-            // If the current node has not been visited, mark it as visited
+           
             if (!visited[currentNode]) {
                 visited[currentNode] = true;
                 
-                // Push all adjacent nodes to the stack
+        
                 for (int i = 0; i < n; i++) {
                     if (graph[currentNode][i] == 1 && !visited[i]) {
                         stack.push(i);
@@ -30,8 +29,7 @@ public class DFS {
                 }
             }
         }
-        
-        // If the loop ends without finding the target, return false
+     
         return false;
     }
     
@@ -48,7 +46,7 @@ public class DFS {
             {0, 0, 1, 1, 0}
         };
         
-        // Searching for a node in the graph
+      
         int startNode = 0; // Start DFS from node 0
         int targetNode = 4; // Target node to search for
         
